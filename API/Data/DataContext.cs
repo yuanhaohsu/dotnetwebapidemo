53d6aee4d1ffddaa35cfc8dbcf77a6a4b89dc3b8
ryanbc660 <ryanrayn1939@gmail.com>
2024-05-01 17:21:34 +0800

    End of Section_2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options) //Base = DBContext; options will be add #13
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+         //Add this line
+    }
+}
