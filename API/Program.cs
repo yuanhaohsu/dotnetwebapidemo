6eeb8607357e16cfb6e28c503c28e094978ec55a
Nick Hsu <37440978+yuanhaohsu@users.noreply.github.com>
2024-05-02 16:58:51 +0800

    Add files via upload

--- API/Program.cs
+++ API/Program.cs
@@ -9,12 +9,28 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>( opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-// fix by codepolit - opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+// fix by codepoilot - opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 //builder.Services.AddSwaggerGen();
+
+// builder.Services.AddCors(); //add this line to fix the CORS error
+
+//#41 Token Services
+builder.Services.AddScoped<ITokenService, TokenService>(); //addTransient, addScoped, addSingleton
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme) //#44 -> nuget gallery -> Microsoft.AspNetCore.Authentication.JwtBearer
+    .AddJwtBearer(opt =>
+    {
+        opt.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"])),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
 
 var app = builder.Build();
 
@@ -25,9 +41,13 @@ var app = builder.Build();
 //              app.UseSwaggerUI();
 //           }
 
+//app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200")); //add this line to fix the CORS error
+
 app.UseHttpsRedirection();
 
+//#44
 app.UseAuthorization();
+app.UseAuthentication();
 
 app.MapControllers();
 
