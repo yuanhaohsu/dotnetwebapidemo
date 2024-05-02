92d7ba53b1379ae5966112296ff12103bdd50c8b
Nick Hsu <37440978+yuanhaohsu@users.noreply.github.com>
2024-05-02 15:38:30 +0800

    Add files via upload

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -3,10 +3,12 @@
 public class AppUser
 {
     public int Id { get; set; }
+
+    // [Required] #38 dotnet ef migration add UsernameRequired
     public string UserName { get; set; }
 
 //#33
-// dotnet ef migrations add UserPasswordAdded 
+// dotnet ef migrations add UserPasswordAdded
     public byte[] PasswordHash { get; set; }
     public byte[] PasswordSalt { get; set; }
 }
