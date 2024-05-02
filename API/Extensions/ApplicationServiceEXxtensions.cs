7813cad47a905329f9b6c35961c1dac8dc97da67
Nick Hsu <37440978+yuanhaohsu@users.noreply.github.com>
2024-05-02 17:26:01 +0800

    Add files via upload

---
+++ API/Extensions/ApplicationServiceEXxtensions.cs
@@ -0,0 +1,16 @@
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors();
+            services.AddScoped<ITokenService, TokenService>(); //addTransient, addScoped, addSingleton
+            
+            return services;
+        }
+}
