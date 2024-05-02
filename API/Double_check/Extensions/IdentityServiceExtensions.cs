4f3dadd80ef64ecc07e0a49987c9912928e7fa90
Nick Hsu <37440978+yuanhaohsu@users.noreply.github.com>
2024-05-02 17:27:03 +0800

    Add files via upload

---
+++ API/Double_check/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,26 @@
+namespace API.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+            
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme) //#44 -> nuget gallery -> Microsoft.AspNetCore.Authentication.JwtBearer
+                .AddJwtBearer(opt =>
+                {
+                    opt.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"])),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
+            return services;
+        }
+    }
+}
