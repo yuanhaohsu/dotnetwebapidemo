6fc2e6a2f533d59b119b4e1d295527f32fa8efa8
Nick Hsu <37440978+yuanhaohsu@users.noreply.github.com>
2024-05-03 16:26:12 +0800

    Add files via upload

--- 1_Readme/nav.component.ts
+++ 1_Readme/nav.component.ts
@@ -10,12 +10,12 @@ export class NavComponent implements OnInit{
     //#51
         model:any = {};
         //56  - loggedIn: = false;
-        currentUser$ = Observable<User | null> = of(null);
+        //currentUser$ = Observable<User | null> = of(null);
 
-constructor(private acoountService: AccountService) {}
+constructor(public acoountService: AccountService) {}
 //55    but for temporary purposes
     ngOnIni():void{ //56
-        this.currentUser$ = this.acoountService.currentUser$;
+        //this.currentUser$ = this.acoountService.currentUser$;
     }
     //55 , 56
     // getCurrentUser() { //this.acoountService.currentUser$.subscribe({ //next: user => this.loggedIn = !!user; //error: err => console.log(err);}) // }
