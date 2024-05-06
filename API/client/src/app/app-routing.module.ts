53da942cd3878f30ffc6e90e834ea6577a425bb0
Nick Hsu <37440978+yuanhaohsu@users.noreply.github.com>
2024-05-06 16:52:45 +0800

    Add files via upload

--- API/client/src/app/app-routing.module.ts
+++ API/client/src/app/app-routing.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [ //64
-  {path:'', component: HomeComponent},
-  {path:'members', component: MemberListComponent},
-  {path:'members/:id', component: MemberDetailComponent},
-  {path:'lists', component: ListsComponent},
-  {path:'messages', component: MessagesComponent},
-  {path:'**', component: HomeComponent},
-]; 
-
-// Path Matching 
+{path: '', component: HomeComponent},
+{path: '',
+  runGuardsAndResolvers: 'always',
+  canActivate: [authGuard],
+  children: [
+    {path: 'members', component: MemberListComponent},
+    {path: 'members/:id', component: MemberDetailComponent, canActivate: [adminGuard]},
+    {path: 'lists', component: ListsComponent},
+    {path: 'messages', component: MessagesComponent},
+   
+  ]
+},
+{path: '**', component: HomeComponent pathMatch:'full'},// Path Matching 
+];
 
 
 @NgModule({
