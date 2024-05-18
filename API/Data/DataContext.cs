b2e76485c1ed1a8d3f8481cb37e81672401fd399
Nick Hsu <37440978+yuanhaohsu@users.noreply.github.com>
2024-05-18 21:16:51 +0800

    Update DataContext.cs
    Section_14

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,15 +1,35 @@
-﻿using API.Entities;
+using API.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Data
-{
-    public class DataContext: DbContext
-    {
-        public DataContext(DbContextOptions options) : base(options) //Base = DBContext; options will be add #13
-        {
-        }
+namespace API.Data;
 
-        public DbSet<AppUser> Users { get; set; }
-         //Add this line
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
     }
+
+    public DbSet<AppUser> Users { get; set; }
+    public DbSet<UserLike> Likes { get; set; } // #171
+        protected override void OnModelCreating(ModelBuilder builder)
+            {
+                base.OnModelCreating(builder);
+
+                builder.Entity<UserLike>()  //primary key #171
+                    .HasKey(k => new { k.SourceUserId, k.TargetUserId });
+
+                builder.Entity<UserLike>()      //relationshio between source and target user #171
+                    .HasOne(s => s.SourceUser)
+                    .WithMany(l => l.LikedUsers)
+                    .HasForeignKey(s => s.SourceUserId)
+                    .OnDelete(DeleteBehavior.Cascade);
+                    //.OnDelete(DeleteBehavior.NoAction);
+
+                builder.Entity<UserLike>()
+                    .HasOne(s => s.TargetUser)
+                    .WithMany(l => l.LikedByUsers)
+                    .HasForeignKey(s => s.TargetUserId)
+                    .OnDelete(DeleteBehavior.Cascade);
+                    //.OnDelete(DeleteBehavior.NoAction);
+            }
 }
